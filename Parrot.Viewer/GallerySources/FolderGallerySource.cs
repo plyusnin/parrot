169da662a0d58619ab2feeffc74523c499b90357
Evgeniy Plyusnin <eninbox@gmail.com>
2017-12-07 17:02:26 +0500

    Чё-то немного поправил

---
+++ Parrot.Viewer/GallerySources/FolderGallerySource.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using photo.exif;
+using ReactiveUI;
+
+namespace Parrot.Viewer.GallerySources
+{
+    public class FolderGallerySource : IGallerySource
+    {
+        private readonly Parser _parser = new Parser();
+        private readonly string _root;
+        private readonly double _thumbnailSize = 300.0;
+
+        public FolderGallerySource(string Root)
+        {
+            _root = Root;
+            Photos = new ReactiveList<IPhotoEntity>(
+                Directory.EnumerateFiles(_root, "*.jpg")
+                         .Select(OpenPhoto));
+        }
+
+        public IReactiveList<IPhotoEntity> Photos { get; }
+
+        private IPhotoEntity OpenPhoto(string File)
+        {
+            var items = _parser.Parse(File)
+                               .ToDictionary(x => x.Id);
+            var image = Image.FromFile(File);
+
+            var factor = Math.Max(_thumbnailSize / image.Width, _thumbnailSize / image.Height);
+
+            var thumb = new Bitmap(image, new Size((int)(image.Width * factor), (int)(image.Height * factor)));
+            var ms = new MemoryStream();
+            thumb.Save(ms, ImageFormat.Jpeg);
+            return new MemoryPhotoEntity(items, ms.ToArray());
+        }
+    }
+}
--- Parrot.Viewer/GallerySources/IGallerySource.cs
+++ Parrot.Viewer/GallerySources/IGallerySource.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using photo.exif;
 using ReactiveUI;
 
@@ -38,36 +34,5 @@ namespace Parrot.Viewer.GallerySources
         private readonly byte[] _content;
         public MemoryPhotoEntity(Dictionary<int, ExifItem> ExifItems, byte[] Content) : base(ExifItems) { _content = Content; }
         public override Stream Content => new MemoryStream(_content);
-    }
-
-    public class FolderGallerySource : IGallerySource
-    {
-        private readonly Parser _parser = new Parser();
-        private readonly string _root;
-        private readonly double _thumbnailSize = 300.0;
-
-        public FolderGallerySource(string Root)
-        {
-            _root = Root;
-            Photos = new ReactiveList<IPhotoEntity>(
-                Directory.EnumerateFiles(_root, "*.jpg")
-                         .Select(OpenPhoto));
-        }
-
-        public IReactiveList<IPhotoEntity> Photos { get; }
-
-        private IPhotoEntity OpenPhoto(string File)
-        {
-            var items = _parser.Parse(File)
-                               .ToDictionary(x => x.Id);
-            var image = Image.FromFile(File);
-
-            var factor = Math.Max(_thumbnailSize / image.Width, _thumbnailSize / image.Height);
-
-            var thumb = new Bitmap(image, new Size((int)(image.Width * factor), (int)(image.Height * factor)));
-            var ms = new MemoryStream();
-            thumb.Save(ms, ImageFormat.Jpeg);
-            return new MemoryPhotoEntity(items, ms.ToArray());
-        }
     }
 }
