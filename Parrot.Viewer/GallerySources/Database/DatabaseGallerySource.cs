3cd809a8ec299a6bc2770b015093d59a8ba452b3
Plyusnin <eninbox@gmail.com>
2018-01-10 00:09:57 +0500

    Загрузка EXIF и превьюшки в базу разбита на отдельные этапы

--- Parrot.Viewer/GallerySources/Database/DatabaseGallerySource.cs
+++ Parrot.Viewer/GallerySources/Database/DatabaseGallerySource.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Reactive.Concurrency;
 using System.Reactive.Disposables;
@@ -29,7 +30,7 @@ namespace Parrot.Viewer.GallerySources.Database
 
             _core  = Core;
             Photos = _core.Photos
-                          .CreateDerivedCollection(Load, scheduler: TaskPoolScheduler.Default)
+                          .CreateDerivedCollection(Load)
                           .DisposeWith(_disposeOnExit);
         }
 
@@ -39,44 +40,57 @@ namespace Parrot.Viewer.GallerySources.Database
 
         private DatabasePhotoEntity Load(FilePhotoRecord Photo)
         {
-            var fileName = Photo.FileName;
-            var record   = _photos.FindOne(p => p.FileName == fileName);
-            if (record == null)
+            try
             {
-                var exif = _exifManager.Load(fileName);
-                record   = new DbPhotoRecord
-                           {
-                               FileName     = fileName,
-                               Aperture     = exif.Aperture,
-                               Camera       = exif.Camera,
-                               Iso          = exif.Iso,
-                               ShotTime     = exif.ShotTime,
-                               ShutterSpeed = exif.ShutterSpeed,
-                               hasGps       = exif.Gps != null,
-                               Latitude     = exif.Gps?.Latitude.ToE6Int() ?? 0,
-                               Longitude    = exif.Gps?.Longitude.ToE6Int() ?? 0
-                           };
-                _photos.Insert(record);
-                _photos.EnsureIndex(x => x.FileName);
-                var thumbnail = new MemoryStream(_thumbnailFactory.GenerateThumbnail(fileName));
-                _db.FileStorage.Upload($"$/thumbnails/{record.Id}.jpg", $"{record.Id}.jpg",
-                                       thumbnail);
+                Debug.Print($"--> {Photo.FileName}");
+
+                var fileName = Photo.FileName;
+                var record   = _photos.FindOne(p => p.FileName == fileName);
+                if (record == null)
+                {
+                    var exif = _exifManager.Load(fileName);
+                    record   = new DbPhotoRecord
+                               {
+                                   FileName     = fileName,
+                                   Aperture     = exif.Aperture,
+                                   Camera       = exif.Camera,
+                                   Iso          = exif.Iso,
+                                   ShotTime     = exif.ShotTime,
+                                   ShutterSpeed = exif.ShutterSpeed,
+                                   hasGps       = exif.Gps != null,
+                                   Latitude     = exif.Gps?.Latitude.ToE6Int() ?? 0,
+                                   Longitude    = exif.Gps?.Longitude.ToE6Int() ?? 0
+                               };
+                    _photos.Insert(record);
+                    _photos.EnsureIndex(x => x.FileName);
+                }
+                if (!_db.FileStorage.Exists($"$/thumbnails/{record.Id}.jpg"))
+                {
+                    var thumbnail = new MemoryStream(_thumbnailFactory.GenerateThumbnail(fileName));
+                    _db.FileStorage.Upload($"$/thumbnails/{record.Id}.jpg", $"{record.Id}.jpg",
+                                           thumbnail);
+                }
+
+                var gps = record.hasGps
+                              ? new EarthPoint(Degree.FromE6Int(record.Latitude),
+                                               Degree.FromE6Int(record.Longitude))
+                              : (EarthPoint?)null;
+
+                return new DatabasePhotoEntity(_db,
+                                               record.Id,
+                                               fileName,
+                                               new ExifInformation(record.Aperture,
+                                                                   record.ShutterSpeed,
+                                                                   record.Iso,
+                                                                   record.Camera,
+                                                                   record.ShotTime,
+                                                                   gps));
             }
-
-            var gps = record.hasGps
-                          ? new EarthPoint(Degree.FromE6Int(record.Latitude),
-                                           Degree.FromE6Int(record.Longitude))
-                          : (EarthPoint?)null;
-
-            return new DatabasePhotoEntity(_db,
-                                           record.Id,
-                                           fileName,
-                                           new ExifInformation(record.Aperture,
-                                                               record.ShutterSpeed,
-                                                               record.Iso,
-                                                               record.Camera,
-                                                               record.ShotTime,
-                                                               gps));
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
